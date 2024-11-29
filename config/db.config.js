31ee95f92478f9bc73503c98ff83b59ba9d75764
arkadiusoo <arkadiusz.mlyn@icloud.com>
2024-11-29 23:57:36 +0100

    db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,15 @@
-const { Client } = require("pg");
-const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "aji_task_3",
-  password: "arek",
-  port: 5432,
-});
-client.connect();
+require("dotenv").config();
+
+module.exports = {
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "postgres",
+  PASSWORD: process.env.DB_PASSWORD || "",
+  DB: process.env.DB_NAME || "shop",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
