68e434355b6965b0b57935516b96e37f6432b308
arkadiusoo <arkadiusz.mlyn@icloud.com>
2024-11-30 01:03:50 +0100

    categories works

--- models/index.js
+++ models/index.js
@@ -8,13 +8,14 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   pool: dbConfig.pool,
 });
 // creating db object
+const db = {};
 db.Sequelize = Sequelize; // sharing Sequelize class
 db.sequelize = sequelize; // sharing db conection
 // importing models
 db.categories = require("./category.model")(sequelize, DataTypes);
-db.products = require("./product.model")(sequelize, DataTypes);
-db.orders = require("./order.model")(sequelize, DataTypes);
-db.statuses = require("./status.model")(sequelize, DataTypes);
-db.orderProduct = require("./order_product.model")(sequelize, DataTypes);
+// db.products = require("./product.model")(sequelize, DataTypes);
+// db.orders = require("./order.model")(sequelize, DataTypes);
+// db.statuses = require("./status.model")(sequelize, DataTypes);
+// db.orderProduct = require("./order_product.model")(sequelize, DataTypes);
 // exporting db object
 module.exports = db;
